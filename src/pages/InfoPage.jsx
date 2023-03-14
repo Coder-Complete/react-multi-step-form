a1aac50fd4eecb3b13992d6ee19f80e59993e012
Jared <weissjmw@gmail.com>
2023-03-13 21:42:17 -0400

    validation done for info page

--- src/pages/InfoPage.jsx
+++ src/pages/InfoPage.jsx
@@ -1,11 +1,36 @@
-import React, { useId } from "react";
+import React, { useId, useState } from "react";
 import "./InfoPage.css";
 
 function InfoPage({ formFields, handleChange, nextPage }) {
+  const [errors, setErrors] = useState({});
   const id = useId();
 
   function validate() {
-    return true;
+    let errs = {};
+    let returnValue = true;
+    if (formFields.name === "") {
+      errs["name"] = "This field is required";
+      returnValue = false;
+    }
+    if (formFields.email === "") {
+      errs["email"] = "This field is required";
+      returnValue = false;
+    } else if (!formFields.email.includes("@")) {
+      errs["email"] = "@ is required";
+      returnValue = false;
+    } else if (formFields.email.endsWith("@")) {
+      errs["email"] = "There needs to be text after the @ symbol";
+      returnValue = false;
+    }
+    if (formFields.phone === "") {
+      errs["phone"] = "This field is required";
+      returnValue = false;
+    } else if (!new RegExp("^[0-9]*$").test(formFields.phone.trim())) {
+      errs["phone"] = "Only use numbers";
+      returnValue = false;
+    }
+    if (Object.keys(errs).length) setErrors(errs);
+    return returnValue;
   }
 
   function handleSubmit(e) {
@@ -22,6 +47,7 @@ function InfoPage({ formFields, handleChange, nextPage }) {
       <div>
         <label htmlFor={id + "-name"} name={id + "-name"}>
           Name
+          {errors["name"] && <p className="red">{errors["name"]}</p>}
         </label>
         <input
           id={id + "-name"}
@@ -29,19 +55,23 @@ function InfoPage({ formFields, handleChange, nextPage }) {
           type="text"
           onChange={handleChange}
           value={formFields.name}
+          className={errors["name"] ? "input-error" : ""}
         />
         <label htmlFor={id + "-email"} name={id + "-email"}>
           Email Address
+          {errors["email"] && <p className="red">{errors["email"]}</p>}
         </label>
         <input
           id={id + "-email"}
           name="email"
-          type="email"
+          text="email"
           onChange={handleChange}
           value={formFields.email}
+          className={errors["email"] ? "input-error" : ""}
         />
         <label htmlFor={id + "-phone"} name={id + "-phone"}>
           Phone Number
+          {errors["phone"] && <p className="red">{errors["phone"]}</p>}
         </label>
         <input
           id={id + "-phone"}
@@ -49,6 +79,7 @@ function InfoPage({ formFields, handleChange, nextPage }) {
           type="phone"
           onChange={handleChange}
           value={formFields.phone}
+          className={errors["phone"] ? "input-error" : ""}
         />
       </div>
       <button type="submit">Next Step</button>
