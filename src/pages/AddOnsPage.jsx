95250e00dd1ca807ff78e00bc5a15ce6dc1e6953
Jared <weissjmw@gmail.com>
2023-03-13 22:19:08 -0400

    addons page done

--- src/pages/AddOnsPage.jsx
+++ src/pages/AddOnsPage.jsx
@@ -1,9 +1,7 @@
-import React, { useId } from "react";
+import React from "react";
 import "./AddOnsPage.css";
 
 function AddOnsPage({ formFields, handleChange, nextPage, prevPage }) {
-  const id = useId();
-
   function validate() {
     return true;
   }
@@ -20,7 +18,16 @@ function AddOnsPage({ formFields, handleChange, nextPage, prevPage }) {
   return (
     <form className="main-section__form addons-page" onSubmit={handleSubmit}>
       <div className="addons-page__options">
-        <label>
+        <label
+          style={
+            formFields.onlineService
+              ? {
+                  border: "1px solid var(--dark-purple)",
+                  backgroundColor: "var(--selected-background)",
+                }
+              : {}
+          }
+        >
           <input
             type="checkbox"
             name="onlineService"
@@ -31,9 +38,20 @@ function AddOnsPage({ formFields, handleChange, nextPage, prevPage }) {
             <p className="blue">Online service</p>
             <p>Access to multiplayer games</p>
           </div>
-          <span className="purple">+$1/mo</span>
+          <span className="purple">
+            {formFields.yearly ? "+$10/yr" : "+$1/mo"}
+          </span>
         </label>
-        <label>
+        <label
+          style={
+            formFields.largerStorage
+              ? {
+                  border: "1px solid var(--dark-purple)",
+                  backgroundColor: "var(--selected-background)",
+                }
+              : {}
+          }
+        >
           <input
             type="checkbox"
             name="largerStorage"
@@ -44,9 +62,20 @@ function AddOnsPage({ formFields, handleChange, nextPage, prevPage }) {
             <p className="blue">Larger storage</p>
             <p>Extra 1TB of cloud save</p>
           </div>
-          <span className="purple">+$2/mo</span>
+          <span className="purple">
+            {formFields.yearly ? "+$20/yr" : "+$2/mo"}
+          </span>
         </label>
-        <label>
+        <label
+          style={
+            formFields.customizableProfile
+              ? {
+                  border: "1px solid var(--dark-purple)",
+                  backgroundColor: "var(--selected-background)",
+                }
+              : {}
+          }
+        >
           <input
             type="checkbox"
             name="customizableProfile"
@@ -57,7 +86,9 @@ function AddOnsPage({ formFields, handleChange, nextPage, prevPage }) {
             <p className="blue">Customizable Profile</p>
             <p>Custom theme on your profile</p>
           </div>
-          <span className="purple">+$2/mo</span>
+          <span className="purple">
+            {formFields.yearly ? "+$20/yr" : "+$2/mo"}
+          </span>
         </label>
       </div>
       <div className="addons-page__button-container">
